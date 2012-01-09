fcefe52daffc82d5cccd8da5b90891ebccc603d2
Jesper Blad Jensen <deldy@deldysoft.dk>
2012-01-09 18:39:43 +0100

    API lockdown

--- app/controllers/api/v1/meetings_controller.rb
+++ app/controllers/api/v1/meetings_controller.rb
@@ -2,6 +2,29 @@ class Api::V1::MeetingsController < ApplicationController
   respond_to :json
   def index
     @meetings = Meeting.upcomming.approved.order("starts_at")
-    respond_with @meetings
+    respond_with meetings_json(@meetings)
+  end
+
+  private
+
+  def meetings_json(meetings)
+    {
+      Meetings: meetings.collect do |meeting|
+        meeting_json(meeting)
+      end
+    }
+  end
+
+  def meeting_json(meeting)
+    {
+      ID: meeting.id,
+      Title: meeting.title,
+      DateStart: meeting.starts_at,
+      Summary: meeting.description,
+      Location: meeting.location,
+      Link: meeting.url,
+      Organizer: meeting.organizer,
+      Organiser: meeting.organizer # DEPRECATED: for windows phone support (spelled wrong)
+    }
   end
 end
