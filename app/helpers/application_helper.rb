3600f1e3c69cc5d69b3a5fda096edea3ff0a67a6
Jesper Blad Jensen <deldy@deldysoft.dk>
2012-01-13 13:56:32 +0100

    New line support

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,12 @@ module ApplicationHelper
     
     <<-eos
       <strong>#{meeting.location}, #{l meeting.starts_at, :format => :long} - #{meeting.organizer}</strong><br/> 
-      <p>#{meeting.description}</p>
+      <p>#{nl2br h(meeting.description)}</p>
       eos
           
   end
+
+  def nl2br(s)
+     raw h(s).gsub(/\n/, '<br/>')
+ end
 end
