be43bee9d3e7d55779c2238e6ad94cc25f1eff82
Jesper Blad Jensen <deldy@deldysoft.dk>
2012-01-21 09:26:28 +0100

    share buttons

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,11 @@ class ApplicationController < ActionController::Base
   APP_DOMAIN = 'geekhub.dk'
 
   def ensure_domain
-    if request.env['HTTP_HOST'] != APP_DOMAIN
-      # HTTP 301 is a "permanent" redirect
-      redirect_to "http://#{APP_DOMAIN}", :status => 301
+    if Rails.env == "production"
+      if request.env['HTTP_HOST'] != APP_DOMAIN
+        # HTTP 301 is a "permanent" redirect
+        redirect_to "http://#{APP_DOMAIN}", :status => 301
+      end
     end
   end
 end
