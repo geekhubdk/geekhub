9b1118eb8a3d9402d05d4d1f06c535204908b3c3
Jesper Blad Jensen <deldy@deldysoft.dk>
2012-04-05 11:47:46 +0200

    Relayout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @newest_approved = Meeting.approved.order("approved_at desc").limit(5)
+    @newest_approved = Meeting.approved.order("approved_at desc").limit(3)
     @all_upcomming = Meeting.approved.upcomming.order("starts_at")
   end
 end
