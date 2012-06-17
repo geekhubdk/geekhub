ad853d85adc9d61039f179a9fca948b3188b5326
Jesper Blad Jensen <deldy@deldysoft.dk>
2012-06-17 17:01:31 +0200

    Mail work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,16 @@ Geekhub::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  # Configuration for using SendGrid on Heroku
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => "geekhub.dk",
+    :address => "smtp.sendgrid.net",
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
