84a46f305760701fbc2f62d8a560163637889453
Jesper Blad Jensen <deldy@deldysoft.dk>
2012-06-17 16:56:24 +0200

    Mail work

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,12 +1,9 @@
-# Disable delivery errors, bad email addresses will be ignored
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.default_url_options = { :host => '##YOUR_PROJECTNAME##.heroku.com' }
 ActionMailer::Base.smtp_settings = {
-  :address    => "smtp.sendgrid.net",
-  :port       => 25,
-  :user_name  => ENV['SENDGRID_USERNAME'],
-  :password   => ENV['SENDGRID_PASSWORD'],
-  :domain     => ENV['SENDGRID_DOMAIN'],
-  :authentication  => :plain
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => ENV['SENDGRID_DOMAIN']
 }
+ActionMailer::Base.delivery_method = :smtp
