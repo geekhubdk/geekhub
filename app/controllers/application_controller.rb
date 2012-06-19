c6ee0cd80078b0cf8ea1d97b74bc9764afa2cb2d
Jesper Blad Jensen <deldy@deldysoft.dk>
2012-06-19 09:40:30 +0200

    API now works from heroku domain also

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ protected
 
   def ensure_domain
     if Rails.env == "production"
-      if request.env['HTTP_HOST'] != APP_DOMAIN
+      if request.env['HTTP_HOST'] != APP_DOMAIN && !request.path.include?('/api/v1')
         # HTTP 301 is a "permanent" redirect
         redirect_to "http://#{APP_DOMAIN}", :status => 301
       end
