b0987918bec7be483542b5c850e5728076d26859
Jesper Blad Jensen <deldy@deldysoft.dk>
2012-06-21 11:16:30 +0200

    Refactored toolbar, and added some new icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,6 +22,6 @@ module ApplicationHelper
 
   def avatar_url(user)
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=16"
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=24"
   end
 end
