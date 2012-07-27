3abd2578611ffdcc3ce63035e4f7fcad753742c7
Jesper Blad Jensen <deldy@deldysoft.dk>
2012-07-27 20:48:12 +0200

    Added some test for the location filter

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -12,4 +12,24 @@ class HomeControllerTest < ActionController::TestCase
   	assert_not_nil assigns(:location_filters)
   	assert_true assigns(:location_filters).any?{|v| v == meetings(:one).location}
   end
+
+  test "one location filter should work" do
+    location = meetings(:one).location
+    get :index, location: location
+
+    assert_not_nil assigns(:location_filters)
+    assert_true assigns(:location_filters).any?{|v| v == location}
+    assert_true assigns(:meetings).any?{|m| m.location = location}
+    assert_false assigns(:meetings).any?{|m| m.location != location}
+  end
+
+  test "two location filter should work" do
+    locations = [meetings(:one).location,meetings(:two).location]
+    get :index, location: locations
+
+    assert_not_nil assigns(:location_filters)
+    assert_true assigns(:location_filters).any?{|v| (v == locations[0]) || (v == locations[1])}
+    assert_false assigns(:meetings).any?{|m| (m.location != locations[0] && m.location != locations[1])}
+  end
+
 end
