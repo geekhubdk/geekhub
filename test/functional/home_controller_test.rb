9a346fa40f7907917e274d5972cfed6dda55d468
Jesper Blad Jensen <deldy@deldysoft.dk>
2012-07-27 20:51:41 +0200

    Added tests for the organizer filter

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -17,7 +17,6 @@ class HomeControllerTest < ActionController::TestCase
     location = meetings(:one).location
     get :index, location: location
 
-    assert_not_nil assigns(:location_filters)
     assert_true assigns(:location_filters).any?{|v| v == location}
     assert_true assigns(:meetings).any?{|m| m.location = location}
     assert_false assigns(:meetings).any?{|m| m.location != location}
@@ -27,9 +26,23 @@ class HomeControllerTest < ActionController::TestCase
     locations = [meetings(:one).location,meetings(:two).location]
     get :index, location: locations
 
-    assert_not_nil assigns(:location_filters)
     assert_true assigns(:location_filters).any?{|v| (v == locations[0]) || (v == locations[1])}
     assert_false assigns(:meetings).any?{|m| (m.location != locations[0] && m.location != locations[1])}
+  end
+
+  test "one organizer filter should work" do
+    organizer = meetings(:one).organizer
+    get :index, organizer: organizer
+
+    assert_true assigns(:meetings).any?{|m| m.organizer = organizer}
+    assert_false assigns(:meetings).any?{|m| m.organizer != organizer}
+  end
+
+  test "two organizer filter should work" do
+    organizers = [meetings(:one).organizer,meetings(:two).organizer]
+    get :index, organizer: organizers
+
+    assert_false assigns(:meetings).any?{|m| (m.organizer != organizers[0] && m.organizer != organizers[1])}
   end
 
 end
