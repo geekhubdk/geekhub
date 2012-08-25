b3889007698efd69ebfc9d77c2092ef3a753a34b
Jesper Blad Jensen <deldy@deldysoft.dk>
2012-08-25 11:17:09 +0200

    Heroku fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,11 +11,14 @@ Geekhub::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = true
 
+
+  config.assets.initialize_on_precompile=false
+  
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
