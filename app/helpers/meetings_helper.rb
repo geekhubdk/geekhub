36b733245165665c971a0b0bfaf8886b3c63ca0d
Jesper Blad Jensen <deldy@deldysoft.dk>
2012-09-03 22:11:16 +0200

    Date formatting

--- app/helpers/meetings_helper.rb
+++ app/helpers/meetings_helper.rb
@@ -1,2 +1,15 @@
+# encoding: utf-8
+#
 module MeetingsHelper
+  def friendly_date date
+    str = l date, format: :short
+
+    if date.to_date < 7.days.from_now.to_date
+      week_day_name = l(date, :format => '%A')
+      "På #{week_day_name}, den #{str}"
+    else
+      str
+    end
+
+  end
 end
