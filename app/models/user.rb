56c5185023104610134f60f53965c1e10a86f287
Jesper Blad Jensen <deldy@deldysoft.dk>
2012-10-05 00:21:23 +0200

    Removed unused function

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,22 +8,4 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me
 
   validates_uniqueness_of :email
-
-  def self.authenticate email, password
-    if email.blank? || password.blank?
-      false
-    end
-    
-    user = User.find_for_database_authentication(:email=>email)
-    
-    if user.nil?
-      false
-    end
-
-    if user.valid_password?(password)
-      user
-    else
-      false
-    end
-  end
 end
