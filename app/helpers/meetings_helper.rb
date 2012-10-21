f72fbbb5c8c4f4470d993947b4f50aaa2c1cd9ae
Jesper Blad Jensen <deldy@deldysoft.dk>
2012-10-21 16:26:50 +0200

    Better date formatting

--- app/helpers/meetings_helper.rb
+++ app/helpers/meetings_helper.rb
@@ -4,9 +4,11 @@ module MeetingsHelper
   def friendly_date date
     str = l date, format: :short
 
-    if date.to_date < 7.days.from_now.to_date
+    if date.to_date < 7.days.from_now.to_date && date.to_date > 1.day.from_now.to_date
       week_day_name = l(date, :format => '%A')
       "På #{week_day_name}, den #{str}"
+    elsif date.to_date == Date.today
+      "I dag, den #{str}"
     else
       str
     end
