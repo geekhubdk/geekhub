fc8e2cecad5cc7e6ea37bf8ef6a2b90f5c7bacc4
Jesper Blad Jensen <deldy@deldysoft.dk>
2012-12-04 17:43:39 +0100

    Added test to meeting suggestions

--- test/functional/meeting_suggestions_controller_test.rb
+++ test/functional/meeting_suggestions_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class MeetingSuggestionsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create meeting sugestion" do
+    assert_difference('MeetingSuggestion.count') do
+      post :create, url: "http://google.dk"
+    end
+
+    assert_redirected_to root_path()
+  end
 end
