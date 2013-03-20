a975f97ba26f4c5e0cf8823cff9cc5e2f491e540
Jesper Blad Jensen <deldy@deldysoft.dk>
2013-03-20 21:57:33 +0100

    Removed test

--- test/functional/meeting_alert_mailer_test.rb
+++ test/functional/meeting_alert_mailer_test.rb
@@ -1,12 +1,5 @@
 require 'test_helper'
 
 class MeetingAlertMailerTest < ActionMailer::TestCase
-  test "alert" do
-    mail = MeetingAlertMailer.alert
-    assert_equal "Alert", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
-  end
 
 end
