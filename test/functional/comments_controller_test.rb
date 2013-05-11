6632b07e29f5302900635186cc23f58ad0108f42
Jesper Blad Jensen <deldy@deldysoft.dk>
2013-05-11 13:38:02 +0200

    Added tests for meeting subscription controller

--- test/functional/comments_controller_test.rb
+++ test/functional/comments_controller_test.rb
@@ -6,6 +6,14 @@ class CommentsControllerTest < ActionController::TestCase
     assert_difference "Comment.count", +1 do
       post :create, meeting_id: meetings(:one).id, comment: { name: 'Jesper', email: 'deldy@deldysoft.dk', content: 'hello world!'}
     end
+    
+    assert_redirected_to meeting_path(meetings(:one).id)
+  end
+  
+  test "Create comment - redirects to meeting, if invalid" do
+    post :create, meeting_id: meetings(:one).id, comment: { name: 'Jesper', email: 'deldy@deldysoft.dk'}
+    
+    assert_redirected_to meeting_path(meetings(:one).id)
   end
 
 end
--- test/unit/meeting_email_alert_subscription_test.rb
+++ test/unit/meeting_email_alert_subscription_test.rb
@@ -1,7 +1,5 @@
 require 'test_helper'
 
 class MeetingEmailAlertSubscriptionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
 end
