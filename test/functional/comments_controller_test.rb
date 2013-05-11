4535cea9cd3bee95d6d6ab6815250f7ffef18945
Jesper Blad Jensen <deldy@deldysoft.dk>
2013-05-11 13:12:50 +0200

    Added comment controller test

--- test/functional/comments_controller_test.rb
+++ test/functional/comments_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "Create comment" do
+    assert_difference "Comment.count", +1 do
+      post :create, meeting_id: meetings(:one).id, comment: { name: 'Jesper', email: 'deldy@deldysoft.dk', content: 'hello world!'}
+    end
+  end
+
 end
