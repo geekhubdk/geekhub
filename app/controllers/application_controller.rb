5e17706373d9953de93329dc0e36c28584ba33f7
Jesper Blad Jensen <deldy@deldysoft.dk>
2013-06-05 21:14:58 +0200

    Removed ensure domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :ensure_domain
-
 protected 
 
-  def ensure_domain
-    if Rails.env == "production"
-      if request.env['HTTP_HOST'] == 'geekhub.dk' && !request.path.include?('/api/v1')
-        # HTTP 301 is a "permanent" redirect
-        redirect_to "http://geekhub.herokuapp.com", :status => 302
-      end
-    end
-  end
-  
   def redirect_to_login
     redirect_to new_user_session_path
   end
