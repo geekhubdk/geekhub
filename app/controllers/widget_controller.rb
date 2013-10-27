e4cce999018bae46c6338ba365999fe01c9ebd9d
Jesper Blad Jensen <deldy@deldysoft.dk>
2013-10-27 15:14:10 +0100

    Fixed widget solution

--- app/controllers/widget_controller.rb
+++ app/controllers/widget_controller.rb
@@ -1,5 +1,6 @@
 class WidgetController < ApplicationController
   def show
+    headers['X-Frame-Options'] = "ALLOWALL"
     filter = Meeting.filter(params)
     @meetings = filter.meetings.take(5)
     @title = params[:title] || "Udvikler events i danmark"
