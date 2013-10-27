83fcca97b82a37b5fa5ad06ca2b6a9e9774498a4
Jesper Blad Jensen <deldy@deldysoft.dk>
2013-10-27 19:18:57 +0100

    Moved tests to their right place

--- test/unit/meeting_test.rb
+++ test/unit/meeting_test.rb
@@ -146,4 +146,17 @@ class MeetingTest < ActiveSupport::TestCase
     m2 = Meeting.find(m.id)
     assert m2.tag_names = ".net,ruby on rails"
   end
+
+  test "creating a meeting with a list of tags" do
+    m = meetings(:future)
+    m.meeting_tags.create(tag: Tag.find_or_create_by(name: ".NET"))
+    m.meeting_tags.create(tag: Tag.find_or_create_by(name: "JavaScript"))
+    assert m.save
+  end
+
+  test "getting a list of meetings with a any of the supplied tags" do
+    meetings = Meeting.filter({all: "1", tag:[".NET", "JavaScript"]}).meetings
+    assert_equal 2, meetings.length
+  end
+
 end
