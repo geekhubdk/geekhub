1c8042376fea1efa611c44e6eed48bf7e92cdb49
Jesper Blad Jensen <deldy@deldysoft.dk>
2013-10-28 16:27:11 +0100

    Large widget mode

--- app/controllers/widget_controller.rb
+++ app/controllers/widget_controller.rb
@@ -4,7 +4,8 @@ class WidgetController < ApplicationController
     filter = Meeting.filter(params)
     @meetings = filter.meetings.take(5)
     @title = params[:title] || "Udvikler events i danmark"
-    render :show, :layout => false 
+    @mode = params[:mode] || "small"
+    render @mode, :layout => false
   end
 
   def example
