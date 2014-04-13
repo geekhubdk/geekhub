bb0c9d5fa8b1b947b825f8acaf3be4412cf756c9
Jesper Blad Jensen <deldy@deldysoft.dk>
2014-04-13 13:20:08 +0200

    Added newsletter count to subscribe page

--- Geekhub/App/Controllers/NewsletterSubscriptionsController.cs
+++ Geekhub/App/Controllers/NewsletterSubscriptionsController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Web.Mvc;
 using Deldysoft.Foundation.CommandHandling;
-using Geekhub.App.Core.CommandHandling;
 using Geekhub.App.Modules.Alerts.Commands;
+using Geekhub.App.Modules.Alerts.Queries;
 using Geekhub.App.Modules.Alerts.Support;
 
 namespace Geekhub.App.Controllers
@@ -9,22 +9,26 @@ namespace Geekhub.App.Controllers
     public class NewsletterSubscriptionsController : ControllerBase
     {
         private readonly ICommandExecuter _commandExecuter;
+        private readonly SubscriberCountQuery _subscriberCountQuery;
 
-        public NewsletterSubscriptionsController(ICommandExecuter commandExecuter)
+        public NewsletterSubscriptionsController(ICommandExecuter commandExecuter, SubscriberCountQuery subscriberCountQuery)
         {
             _commandExecuter = commandExecuter;
+            _subscriberCountQuery = subscriberCountQuery;
         }
 
         [Route("newsletter/subscribe")]
         public ActionResult Create()
         {
+            IncludeSubscriberCount();
             return View();
         }
-        
+
         [Route("newsletter/subscribe")]
         [HttpPost]
         public ActionResult Create(string email)
         {
+            IncludeSubscriberCount();
             if (string.IsNullOrEmpty(email)) {
                 return View();
             }
@@ -59,6 +63,11 @@ namespace Geekhub.App.Controllers
             Notice("Du er nu afmeldt nyhedsbreve");
 
             return Redirect("~/");
+        }
+        
+        private void IncludeSubscriberCount()
+        {
+            ViewBag.SubscriberCount = _subscriberCountQuery.Execute();
         }
 
 	}
---
+++ Geekhub/App/Modules/Alerts/Queries/SubscriberCountQuery.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Geekhub.App.Core.CommandHandling;
+using Geekhub.App.Core.Data;
+
+namespace Geekhub.App.Modules.Alerts.Queries
+{
+    public class SubscriberCountQuery : QueryBase
+    {
+        public SubscriberCountQuery(DataContext dataContext) : base(dataContext)
+        {
+        }
+
+        public int Execute()
+        {
+            return DataContext.NewsletterSubscriptions.Count(x => x.SubscribedToNewMeetingUpdates);
+        }
+    }
+}
