a81602db2c423ca97381e1febe16923b965c8727
Jesper Blad Jensen <deldy@deldysoft.dk>
2014-05-03 10:42:04 +0200

    Basic map

---
+++ Geekhub/App/Controllers/MeetingsMapController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Geekhub.App.Controllers
+{
+    public class MeetingsMapController : Controller
+    {
+        [Route("Map")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
